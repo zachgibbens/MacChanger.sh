b52bf884b8b2037266a128d6f643c2dbe1b9c00e
Zachariah Gibbens <zachgibbens@zachgibbens.org>
2019-03-19 00:18:33 +0000

    Updated oui.txt from http://standards-oui.ieee.org/oui.txt

--- macchanger.sh
+++ macchanger.sh
@@ -4,5 +4,7 @@ FILE=/root/oui.txt
 PREFIX=$(cat $FILE | grep base\ 16 | shuf -n 1|cut -d"(" -f1|cut -d" " -f1)
 SUFFIX=$(openssl rand -hex 3)
 MACADDR=$(echo $PREFIX$SUFFIX | sed -e 's/.\{2\}/&:/g;s/.$//')
+ip link set dev $DEV down
+sleep 4
 ip link set dev $DEV down address $MACADDR
 
